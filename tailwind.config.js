0f725ae5d374a476308ab2f9e8505da85c2fbc01
vilaysackchantharbounmy <105557606+vilaysackchantharbounmy@users.noreply.github.com>
2022-06-04 21:34:55 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      height: {
+        500: "500px",
+      },
+    },
+  },
+  plugins: [],
+};
